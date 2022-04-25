7accce6167389e0c9d6bb328cd909175604081ab
jroberto-inv <jroberto@inv.com.vc>
2022-04-25 10:32:26 -0300

    on each site request, block it if it's in toggleSitesList

--- assets/js/background.js
+++ assets/js/background.js
@@ -20,6 +20,35 @@ chrome.storage.sync.get([
   toggleSitesList = result.toggleSitesList
 })
 
+// on each site request, block it if it's in toggleSitesList
+chrome.webRequest.onBeforeRequest.addListener(
+  (details) => {
+
+    // if the toggle is inactive, don't block anything
+    if (!toggleSitesActive) {
+      return { cancel: false }
+    }
+
+    // determine if the url is in toggleSitesList
+    let cancel = toggleSitesList.split(/\n/)
+      .some(site => {
+        let url = new URL(details.url)
+        return Boolean(url.hostname.indexOf(site) !== -1)
+      })
+
+    return { cancel: cancel }
+
+  },
+
+  {
+    urls: ["<all_urls>"]
+  },
+  [
+    "blocking"
+  ]
+)
+
+
 
 
 
