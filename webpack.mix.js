c527ff69e83fb72dd46f2ecf1e9952e05e66320d
jroberto-inv <jroberto@inv.com.vc>
2022-04-25 10:58:16 -0300

    add background.js in worrkflow files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix')
 
 mix.setPublicPath('./')
   .sass('assets/sass/popup.scss', 'dist/css')
+  .js('assets/js/background.js', 'dist/js')
   .options({
     processCssUrls: false
   })
