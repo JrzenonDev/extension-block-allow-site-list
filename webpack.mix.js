3f843abbc87f3f6c05935841203f40c340636b17
jroberto-inv <jroberto@inv.com.vc>
2022-04-27 00:29:54 -0300

    register popup.vue in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix')
 mix.setPublicPath('./')
   .sass('assets/sass/popup.scss', 'dist/css')
   .js('assets/js/background.js', 'dist/js')
+  .js('assets/js/popupjs', 'dist/js').vue()
   .options({
     processCssUrls: false
   })
